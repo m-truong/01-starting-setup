8e53e9a26b9ad15c5a2c8fb833858ea639524015
Michael Truong <59580072+m-truong@users.noreply.github.com>
2024-09-06 12:55:16 -0400

    add notes on NgRx

--- src/app/counter-output/counter-output.component.ts
+++ src/app/counter-output/counter-output.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Observable, Subscription } from 'rxjs';
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 // Note: That by simply using a Store, we can remove the CoutnerServiceSubscription to wathc the state of the counter
 // INSTEAD that app-wide counter-state is being maintained VIA THE STORE
@@ -11,13 +11,26 @@ import { Store } from '@ngrx/store';
   templateUrl: './counter-output.component.html',
   styleUrls: ['./counter-output.component.css'],
 })
-export class CounterOutputComponent implements OnInit, OnDestroy {
+export class CounterOutputComponent {
   // counter = 0;
   // counterServiceSub?: Subscription;
 
   // Must DECORATE with $-sign if those properties STORE an OBSERVABLE
+  // Notice that the Observable is a Generic-Type and then the actual type is "number"
   count$: Observable<number>;
 
-  constructor(private store: Store) {}
+  // TODO: output the counter app-wide state here using the store$
 
+  // TS compiling error doesn't KNOW the TYPE returned BY THE observable count$
+  // SO MUST DEFINE Store type inside THE CONSTRUCTOR DEFINITION
+  constructor(private store: Store<{counter: number}>) {
+    // inside the constructor when initializing the CounterOutputComponent
+    // You are initializing an internal property to the CounterOutputComponent instance using 'this.' property
+    // then assigning the this.count$ (Observable) syntax
+    // then the call store.select('') method will ALLOW this component to SELECT a PIECE of the STORE's MANAGED STATE
+    // which is TIED to the COUNTER-REDUCER managing THAT STATE
+    this.count$=store.select('counter')
+  }
+
+  // Best practice is to always unsubscribe to any given observable
 }
