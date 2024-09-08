d7ae333222701424c1acf401100a2138e5613e85
Michael Truong <59580072+m-truong@users.noreply.github.com>
2024-09-08 18:42:19 -0400

    fix all bugFixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,9 +4,7 @@ import { initActionEvent } from './store/counter.actions';
 
 @Component({
     selector: 'app-root',
-    standalone: true,
     // remember must always add new components that are INSIDE the TEMPLATE of the 'standalone angular component'
-    imports: [],
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
 })
